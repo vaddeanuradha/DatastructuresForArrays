b9ee3e58e66313731a6fafd8ca0b98f9b9c67fa2
Anuradha <Anuradha@ANURADHA>
2019-04-29 13:33:06 -0700

    Initial Commit

---
+++ FindMissingNumProblem2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FindMissingNumProblem2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Arr = { 1, 3, 2, 5, 4, 7, 9, 8 };
+            int Size = Arr.Length;
+            int ExpectedSum = (Size + 1) * (Size + 2) / 2;
+            for (int i =0;i<Size;i++)
+            {
+                ExpectedSum -= Arr[i];
+            }
+
+            Console.WriteLine("The missing number is {0}", ExpectedSum);
+            Console.Read();
+        }
+    }
+}
---
+++ SubArrayLargestSum/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace SubArrayLargestSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { -2, 3, 4, -3, -2, 1, 5, -3 };
+
+            int size = arr.Length;
+            int Max = int.MinValue,
+                CurrentMax = 0, start=0,end=0,t=0;
+
+            for (int i = 0; i < size; i++)
+            {
+                CurrentMax = CurrentMax + arr[i];
+
+                if (Max < CurrentMax)
+                {
+                    Max = CurrentMax;
+                    start = t;
+                    end = i;
+                }
+                if (CurrentMax < 0)
+                {
+                    CurrentMax = 0;
+                    t = i + 1;
+                }
+            }
+      
+            Console.WriteLine("The Maximum sum of Contiguous SubArray {0}", Max);
+            Console.WriteLine("Starting Index {0}", start);
+            Console.WriteLine("Ending Index {0}", end);
+
+            Console.ReadLine();
+        }
+    }
+}
